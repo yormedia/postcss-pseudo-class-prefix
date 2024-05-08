87f4e9ead84b48a1c6cf58cdcf50bb373cd00c56
yormedia <info@yormedia>
2024-05-08 18:02:10 +0200

    Test

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * @type {import('postcss').PluginCreator}
+ */
+module.exports = (opts = {}) => {
+  // Work with options here
+
+  return {
+    postcssPlugin: 'postcss-pseudo-class-prefix',
+    Once(root) {
+      root.walkRules(rule => {
+        rule.selectors = rule.selectors.map(selector => {
+
+        opts.forEach((prefix) => {
+            let regVar = new RegExp(String.raw`/\b:${prefix}(\w+)`, "g");
+            // Replace each prefix with the respective pseudo-class
+            selector = selector.replace(regVar, '.$1:' + prefix);
+            return selector;
+          });
+        });
+      })
+    }
+  }
+}
+
+module.exports.postcss = true
