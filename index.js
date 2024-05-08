a8a5149fa2fb46f78946f980f02bae4d19bd0bd7
yormedia <info@yormedia>
2024-05-08 18:49:01 +0200

    Third Commit

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 /**
  * @type {import('postcss').PluginCreator}
  */
-module.exports = (opts = {}) => {
-  // Work with options here
 
+module.exports = (opts = []) => { // Assuming opts is an array of prefixes
   return {
     postcssPlugin: 'postcss-pseudo-class-prefix',
     Once(root) {
       root.walkRules(rule => {
         rule.selectors = rule.selectors.map(selector => {
-
+          // Process each prefix
           opts.forEach(prefix => {
-            let regVar = new RegExp(String.raw`/\b${prefix}(\w+)`, "g");
-            // Replace each prefix with the respective pseudo-class
-            selector = selector.replace(regVar, ':$1');
-            return selector;
+            let regVar = new RegExp(`\\b${prefix}(\\w+)`, "g"); // Corrected RegExp
+            selector = selector.replace(regVar, `.$1:${prefix}`); // Correct placement and corrected logic
           });
+          return selector; // Return should be here, after all replacements
         });
-      })
+      });
     }
-  }
-}
+  };
+};
 
-module.exports.postcss = true
+module.exports.postcss = true; // This is fine if using CommonJS
